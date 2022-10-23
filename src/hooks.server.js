f215f627a847e38067cd85b4f90e5367df826818
cnxisne3019 <natthawut.plbc@gmail.com>
2022-10-23 21:55:21 +0700

    please fix authStore to persist data

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,4 +1,5 @@
 import PocketBase from 'pocketbase';
+import { currentUser } from '$lib/store/authStore';
 
 export async function handle({ event, resolve }) {
 	event.locals.pb = new PocketBase('http://localhost:8090');
@@ -7,7 +8,9 @@ export async function handle({ event, resolve }) {
 	event.locals.pb.authStore.loadFromCookie(event.request.headers.get('cookie') || '');
 
 	if (event.locals.pb.authStore.isValid) {
+		console.log();
 		event.locals.user = event.locals.pb.authStore.model;
+		// console.log(event.locals.user);
 	}
 
 	const response = await resolve(event);
@@ -18,9 +21,14 @@ export async function handle({ event, resolve }) {
 	response.headers.set(
 		'set-cookie',
 		event.locals.pb.authStore.exportToCookie({
-			secure: false
+			secure: false,
 		})
 	);
+
+	currentUser.set({
+		id: event.locals.user.id,
+		name: event.locals.user.profile.name,
+	});
 
 	return response;
 }
---
+++ src/lib/store/authStore.js
@@ -0,0 +1,6 @@
+import { writable } from "svelte/store";
+
+export const currentUser = writable({
+  id: '',
+  name: ''
+})
