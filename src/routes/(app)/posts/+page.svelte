5b15b4933b0086e2eb6ade5f73f037d19d64f9ac
cnxisne3019 <cnxisne3019@gmail.com>
2022-10-25 16:44:15 +0700

    migrate from server-side to client-side

--- src/routes/(app)/posts/+page.svelte
+++ src/routes/(app)/posts/+page.svelte
@@ -1,20 +1,35 @@
 <script>
-	// Receive data from the server as data (Framework utilities)
+	import { onMount } from 'svelte';
+	import { client } from '$lib/pocketbase';
+
+	// Received from /src/route/+layout.server.js as data below
 	export let data;
-	// Store in new var
-	const items = data.res.items;
+	console.log(`received from /src/route/+layout.server.js -> ${data.authProfile}`);
+
+	let todoItems = [];
+
+	onMount(async (event) => {
+		client.beforeSend = function (url, reqConfig) {
+			// For list of the possible reqConfig properties check
+			// https://developer.mozilla.org/en-US/docs/Web/API/fetch#options
+			reqConfig.headers = Object.assign({}, reqConfig.headers, {
+				Authorization: `User ${data.authToken}`,
+			});
+			return reqConfig;
+		};
+		const res = await client.records.getList('todos');
+		todoItems = res.items;
+
+	});
 </script>
 
 <div class="flex justify-between items-center">
 	<h1 class="text-2xl font-semibold">My Posts</h1>
-	<a href='/posts/add-new' class="px-4 py-2 bg-amber-400 text-white font-semibold rounded">Add New</a>
+	<a href="/posts/add-new" class="px-4 py-2 bg-amber-400 text-white font-semibold rounded">Add New</a>
 </div>
-<!-- <div class="w-full px-4 overflow-hidden">
-	<pre>{JSON.stringify(items, null, 2)}</pre>
-</div> -->
 
 <div class=" flex flex-col gap-3 mt-4">
-	{#each items as item}
+	{#each todoItems as item}
 		<div class="p-4 border rounded bg-white">
 			<p>{item.title}</p>
 			<p>{item.description}</p>
