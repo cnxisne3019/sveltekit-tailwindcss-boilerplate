50f0d540e89dc47fdd5840b1d2c2ef6507f0d51f
cnxisne3019 <cnxisne3019@gmail.com>
2022-10-25 16:42:54 +0700

    add types

--- src/routes/(app)/+layout.server.js
+++ src/routes/(app)/+layout.server.js
@@ -1,6 +1,6 @@
-// export const ssr = false;
 import { redirect } from '@sveltejs/kit';
 
+/** @type {import('./$types').LayoutServerLoad} */
 export const load = ({ locals }) => {
 	if (!locals.pb.authStore.isValid) {
 		throw redirect(303, '/auth/login');
