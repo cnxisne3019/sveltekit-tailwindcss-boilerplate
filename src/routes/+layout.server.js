0276828cd5d333882c14bfb35ef9cce91c49ff5a
cnxisne3019 <natthawut.plbc@gmail.com>
2022-10-26 09:29:38 +0700

    add comment

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,6 +1,11 @@
 import { seriealizedNonPOJOs } from '$lib/helpers';
 
 /** @type {import('./$types').LayoutServerLoad} */
+
+// THIS IS ROOT OF USER DATA THAT PASS TO EVERY PAGE
+// -> when use in /routes : access by >> export let data
+// -> when need to access in /lib or /lib/* : access by >> page.data (must import '$app/stores')
+
 export const load = ({ locals }) => {
 	if (locals.user && locals.user.profile && locals.pb.authStore) {
 		return {
