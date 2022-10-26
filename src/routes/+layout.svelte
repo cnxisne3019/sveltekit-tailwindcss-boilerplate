cb3e0af2e9a6668a55ad75329238d83184f5ea07
cnxisne3019 <natthawut.plbc@gmail.com>
2022-10-26 09:29:16 +0700

    not necessary.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,6 @@
 <script>
 	import '../app.css';
 	import Navigation from '$lib/components/Navigation.svelte'
-	export let data;
-
-	// console.log('import data from +layout.server.js -> ' + JSON.stringify(data, null, 2));
 </script>
 
 <Navigation/>
