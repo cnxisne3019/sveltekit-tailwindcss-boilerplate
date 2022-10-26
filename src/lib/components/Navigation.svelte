89638a554879295275956b03f8268af72ad27b67
cnxisne3019 <natthawut.plbc@gmail.com>
2022-10-26 09:05:57 +0700

    user pagedata instead pass data around

---
+++ src/lib/components/Navigation.svelte
@@ -0,0 +1,26 @@
+<script>
+	import { page } from '$app/stores';
+
+	const user = $page.data.authProfile
+
+</script>
+
+<nav class="px-4 py-1 bg-white items-center text-slate-800 justify-between flex border-b border-slate-200">
+	<div class="text-3xl font-bold">
+		<a href="/">Sveltekit</a>
+	</div>
+
+	{#if user}
+		<div class="flex space-x-4 items-center font-medium italic text-slate-600">
+			<p>Hello, {user.name}</p>
+			<form action="/auth/logout" method="post">
+				<button class="px-4 py-2 rounded-md bg-rose-500 font-semibold text-white">Logout</button>
+			</form>
+		</div>
+	{:else}
+		<div class="flex space-x2 items-center space-x-2">
+			<a href="/auth/register">Register</a>
+			<a href="/auth/login" class="px-4 py-2 rounded-md bg-sky-600">Login</a>
+		</div>
+	{/if}
+</nav>
