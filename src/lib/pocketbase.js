a33d96bc2db3becfe91ac3c90c3194ad6c1baf98
cnxisne3019 <natthawut.plbc@gmail.com>
2022-10-26 10:28:58 +0700

    add addAuthHeader function

--- src/lib/pocketbase.js
+++ src/lib/pocketbase.js
@@ -1,3 +1,13 @@
+console.log('Pocketbase.js called');
 import PocketBase from 'pocketbase';
 
 export const client = new PocketBase('http://127.0.0.1:8090');
+
+export function addAuthHeader(accessToken) {
+	client.beforeSend = (url, reqConfig) => {
+		reqConfig.headers = Object.assign({}, reqConfig.headers, {
+			Authorization: 'User ' + accessToken,
+		});
+		return reqConfig;
+	};
+}
